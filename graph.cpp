448e02cce9eb3580055f8a0aee14b1d6a22286f2
Anshul Sharma <anshulsharma427@gmail.com>
2017-07-23 00:21:41 -0400

    Add files via upload

---
+++ graph.cpp
@@ -0,0 +1,39 @@
+/*NAME-ANSHUL SHARMA
+STUDENT ID-136530169
+E-MAIL ID-asharma163@myseneca.ca
+*/
+#include<iostream>
+#include "tools.h"
+#include "graph.h"
+using namespace std;
+
+	void getSamples(int samples[], int noOfSamples){
+		int i;
+		for (i = 0; i < noOfSamples; i++){
+			cout << (i + 1) << "/" << noOfSamples << ": ";
+			samples[i] = getInt(1, 1000000);
+		}
+	}
+	void printBar(int val, int max){
+		int i;
+		for (i = 0; i < 70 * val / max; i++){
+			cout << "*";
+		}
+		cout << " " << val << endl;
+	}
+	void printGraph(int samples[], int noOfSamples){
+		int max = findMax(samples, noOfSamples);
+		cout << "Graph:" << endl;
+		for (int i = 0; i < noOfSamples; i++){
+			printBar(samples[i], max);
+		}
+	}
+	int findMax(int samples[], int noOfSamples){
+		int max = samples[0];
+		int i;
+		for (i = 1; i < noOfSamples; i++){
+			if (max < samples[i]) max = samples[i];
+		}
+		return max < 70 ? 70 : max;
+	}
+
