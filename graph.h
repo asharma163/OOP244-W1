b3f9441e67dad405475573eb7847d353f6e96964
Anshul Sharma <anshulsharma427@gmail.com>
2017-07-23 00:21:54 -0400

    Add files via upload

---
+++ graph.h
@@ -0,0 +1,13 @@
+/*NAME-ANSHUL SHARMA
+STUDENT ID-136530169
+E-MAIL ID-asharma163@myseneca.ca
+*/
+#define MAX_NO_OF_SAMPLES 20
+#ifndef SICT_GRAPH_H__
+#define SICT_GRAPH_H__
+
+void getSamples(int samples[], int noOfSamples);
+void printBar(int val, int max);
+void printGraph(int samples[], int noOfSamples);
+int findMax(int samples[], int noOfSamples);
+#endif
